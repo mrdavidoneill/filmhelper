93035d9e652350538fbd129fb17e0cc41780f7e8
David ONeill <mrdavidoneillll@gmail.com>
2023-02-05 19:53:17 +0100

    First commit
    Custom theme

---
+++ shared/theme.ts
@@ -0,0 +1,8 @@
+import { blue, deepPurple } from "@mui/material/colors";
+import { createTheme } from "@mui/material/styles";
+export const theme = createTheme({
+  palette: {
+    primary: blue,
+    secondary: deepPurple,
+  },
+});
