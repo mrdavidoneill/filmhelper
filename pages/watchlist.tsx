e553a86271a8fe51308ba6359031f2ac278e6f90
David ONeill <mrdavidoneillll@gmail.com>
2023-02-08 22:46:54 +0100

    - Use redux state - wrap filteredItems in useEffect - wait for all items Info to finish before setting to items -- then filter out any unfetched info - Remove unneeded fragment

--- pages/watchlist.tsx
+++ pages/watchlist.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import SearchAppBar from "@/components/searchbar";
-import { ChangeEvent, Fragment, useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import Carousel from "@/components/carousel";
 import FilmInfo from "@/components/filminfo";
 import { FilmInfoType, FilmWatchListType } from "@/shared/types";
@@ -8,33 +8,44 @@ import { Alert, CircularProgress, ThemeProvider } from "@mui/material";
 import { theme } from "@/shared/theme";
 import BottomNavBar from "@/components/bottomnav";
 import { getFilmInfo, getFilmWatchList } from "@/shared/api";
+import { useSelector, useDispatch } from "react-redux";
+import watchListSlice, { selectWatchList } from "@/store/slices/watchListSlice";
 
 export default function WatchListPage() {
+  const state = useSelector(selectWatchList);
+  const dispatch = useDispatch();
+
   const [currentIndex, setCurrentIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [items, setItems] = useState<FilmInfoType[]>([]);
+  const [input, setInput] = useState("");
+  const [filteredItems, setFilteredItems] = useState<FilmInfoType[]>([]);
   const [error, setError] = useState("");
   const [dismissError, setDismissError] = useState(false);
-  const [input, setInput] = useState("");
 
-  const filteredItems = items.filter((item) =>
-    item.Title.toLowerCase().includes(input.toLowerCase())
-  );
+  useEffect(() => {
+    setFilteredItems(
+      items
+        .filter((film) => film.imdbID in state)
+        .filter((item) =>
+          item.Title.toLowerCase().includes(input.toLowerCase())
+        )
+    );
+  }, [input, items, state]);
 
   useEffect(() => {
     async function getWatchList(token: string) {
       setIsLoading(true);
       const filmList: FilmWatchListType[] = await fetchFilmWatchList(token);
-      // Fetch all films in filmlist
-      filmList.forEach(async (film, index) => {
-        const filmInfo = await fetchFilmInfo(film.imdb_id);
-        if (filmInfo) {
-          setItems((prev) => {
-            const clone = [...prev];
-            clone[index] = filmInfo;
-            return clone;
-          });
-        }
+      const filmInfoList = await Promise.all(
+        filmList.map(async (film) => {
+          return await fetchFilmInfo(film.imdb_id);
+        })
+      );
+      // Remove any items who's fetchInfo failed
+      setItems(filmInfoList.filter((filmInfo) => filmInfo) as FilmInfoType[]);
+      filmList.forEach((film) => {
+        dispatch(watchListSlice.actions.addToWatchList(film));
       });
       setIsLoading(false);
     }
@@ -123,20 +134,16 @@ export default function WatchListPage() {
             <CircularProgress />
           </div>
         )}
-        <Fragment>
-          <Carousel
-            setCurrentIndex={handleChangeIndex}
-            images={filteredItems.map(
-              (film: { Poster: string }) => film.Poster
-            )}
-            initialIndex={currentIndex}
-          />
-          <div className="flex flex-col flex-1 overflow-y-auto">
-            {filteredItems.length > 0 && (
-              <FilmInfo film={filteredItems[currentIndex]} />
-            )}
-          </div>
-        </Fragment>
+        <Carousel
+          setCurrentIndex={handleChangeIndex}
+          images={filteredItems.map((film: { Poster: string }) => film.Poster)}
+          initialIndex={currentIndex}
+        />
+        <div className="flex flex-col flex-1 overflow-y-auto">
+          {filteredItems.length > 0 && (
+            <FilmInfo film={filteredItems[currentIndex]} />
+          )}
+        </div>
         <BottomNavBar />
       </div>
     </ThemeProvider>
