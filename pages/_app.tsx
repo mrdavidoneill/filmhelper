5e94c55c109234a0f6f7389a1521c1edbb71cae6
David ONeill <mrdavidoneillll@gmail.com>
2023-02-08 20:32:40 +0100

    Add redux
    Wrap app in provider
    Give access to the store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import { wrapper } from "@/store/store";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, ...rest }: AppProps) {
+  const { store } = wrapper.useWrappedStore(rest);
+  const { pageProps = {} } = rest;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
