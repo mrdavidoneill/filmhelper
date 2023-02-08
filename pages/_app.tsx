7e95f1b6995392731dab4ffb8bfeba850a750262
David ONeill <mrdavidoneillll@gmail.com>
2023-02-08 23:34:30 +0100

    Config for MUI and tailwind
    Inject first as recommended
    - https://mui.com/base/guides/working-with-tailwind-css/

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
 import { wrapper } from "@/store/store";
+import { StyledEngineProvider } from "@mui/material/styles";
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store } = wrapper.useWrappedStore(rest);
   const { pageProps = {} } = rest;
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <StyledEngineProvider injectFirst>
+        <Component {...pageProps} />
+      </StyledEngineProvider>
     </Provider>
   );
 }
