6a57b917e8178656dd12fb9d83246c31c21df0e7
David ONeill <mrdavidoneillll@gmail.com>
2023-02-08 23:33:34 +0100

    Without preflight need body style
    - no margin
    - box sizing border box
    -- else margin all round <body/>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body style={{ margin: "0", boxSizing: "border-box" }}>
         <Main />
         <NextScript />
       </body>
