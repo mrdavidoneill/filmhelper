ddc4278f7b7210f98d2bcac5aa08da52774a92cf
David ONeill <mrdavidoneillll@gmail.com>
2023-02-18 12:05:15 +0100

    Mock local storage using Storage.prototype Mock getFilmWatchList Mock getFilmRatings

--- __tests__/discover.js
+++ __tests__/discover.js
@@ -1,11 +1,14 @@
 const GOOD_SEARCH_FILMS = require("@/shared/api_examples/movie_search.json");
 const BAD_SEARCH_FILMS = require("@/shared/api_examples/movie_search_error.json");
 const GOOD_FILM_INFO = require("@/shared/api_examples/movie_info.json");
+const GOOD_WATCHLIST = require("@/shared/api_examples/get_watchlist.json");
+const GOOD_RATINGS = require("@/shared/api_examples/get_ratings.json");
+const SAMPLE_FILMS = require("@/shared/api_examples/backup_movie.json");
 
 import "@testing-library/jest-dom";
 
 import React from "react";
-import { render, act, fireEvent, waitFor } from "@testing-library/react";
+import { act, fireEvent, waitFor } from "@testing-library/react";
 import { getFilmInfo, searchFilms } from "@/shared/api";
 import DiscoverPage from "@/pages/discover";
 import { renderWithProviders } from "@/shared/testUtils";
@@ -28,21 +31,31 @@ jest.mock("@/shared/api", () => {
     getFilmInfo: jest.fn(() => {
       return Promise.resolve(GOOD_FILM_INFO);
     }),
+    getFilmWatchList: jest.fn(() => {
+      return Promise.resolve(GOOD_WATCHLIST);
+    }),
+    getFilmRatings: jest.fn(() => {
+      return Promise.resolve(GOOD_RATINGS);
+    }),
   };
 });
-const localStorageMock = {
-  getItem: jest.fn().mockReturnValue("token"),
-};
+Storage.prototype.getItem = jest.fn().mockReturnValue("A TOKEN");
 
 describe("DiscoverPage", () => {
-  it("renders without error", () => {
-    global.localStorage = localStorageMock;
-    const { asFragment } = renderWithProviders(<DiscoverPage />);
+  it("renders without error", async () => {
+    const { asFragment, getByRole, getByTestId } = renderWithProviders(
+      <DiscoverPage />
+    );
+    await waitFor(() => {
+      expect(getByRole("progressbar")).toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(getByTestId(SAMPLE_FILMS[0].Poster)).toBeInTheDocument();
+    });
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("handles search and sets items", async () => {
-    global.localStorage = localStorageMock;
     const { getByPlaceholderText } = renderWithProviders(<DiscoverPage />);
 
     const input = getByPlaceholderText("Search…");
@@ -63,7 +76,6 @@ describe("DiscoverPage", () => {
   });
 
   it("handles no search results", async () => {
-    global.localStorage = localStorageMock;
     const { getByPlaceholderText, getByText } = renderWithProviders(
       <DiscoverPage />
     );
