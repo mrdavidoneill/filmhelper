32484c3066372704b1c46f9ecdaea3d09e95b44f
David ONeill <mrdavidoneillll@gmail.com>
2023-02-18 12:20:28 +0100

    Mock onChange function Test 0% = 0 stars Test 100% = 5 stars Test 50% = 2.5 stars

--- __tests__/ratingicon.js
+++ __tests__/ratingicon.js
@@ -3,12 +3,41 @@ import { render, fireEvent } from "@testing-library/react";
 import RatingIcon from "@/components/ratingicon";
 
 describe("MuiRating component", () => {
-  it("should have the right value when the rating is changed", () => {
-    const { getByLabelText } = render(<RatingIcon stars={3} />);
-    expect(getByLabelText("4.5 Stars").checked).toBe(false);
-    fireEvent.click(getByLabelText("3 Stars"));
-    expect(getByLabelText("3 Stars").checked).toBe(true);
-    fireEvent.click(getByLabelText("4.5 Stars"));
-    expect(getByLabelText("4.5 Stars").checked).toBe(true);
+  it("should display no stars for 0%", () => {
+    const onChange = jest.fn();
+    const { getByLabelText } = render(
+      <RatingIcon percentage={0} onChange={onChange} />
+    );
+    const stars = new Set([0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5]);
+    for (const star of stars) {
+      expect(
+        getByLabelText(`${star} Star${star === 1 ? "" : "s"}`).checked
+      ).toBe(false);
+    }
+  });
+
+  it("should display 2.5 stars for 50%", () => {
+    const onChange = jest.fn();
+    const { getByLabelText } = render(
+      <RatingIcon percentage={50} onChange={onChange} />
+    );
+    expect(getByLabelText("2.5 Stars").checked).toBe(true);
+  });
+
+  it("should display 5 stars for 100%", () => {
+    const onChange = jest.fn();
+    const { getByLabelText } = render(
+      <RatingIcon percentage={100} onChange={onChange} />
+    );
+    expect(getByLabelText("5 Stars").checked).toBe(true);
+  });
+
+  it("should send the right value when the rating is changed", () => {
+    const onChange = jest.fn();
+    const { getByLabelText } = render(
+      <RatingIcon percentage={0} onChange={onChange} />
+    );
+    fireEvent.click(getByLabelText("3.5 Stars"));
+    expect(onChange).toHaveBeenCalledWith(3.5 * 20);
   });
 });
