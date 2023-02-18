fb67a4747d5c88788c46952f72a30aff9f3b3092
David ONeill <mrdavidoneillll@gmail.com>
2023-02-18 11:34:30 +0100

    Add waitFor so that images are shown in snapshot

--- __tests__/carousel.js
+++ __tests__/carousel.js
@@ -1,20 +1,27 @@
+import "@testing-library/jest-dom";
+
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, waitFor } from "@testing-library/react";
 import Carousel from "@/components/carousel";
 
 describe("Carousel", () => {
-  it("should match the snapshot", () => {
+  it("should match the snapshot", async () => {
     const images = ["image1.jpg", "image2.jpg"];
     const setCurrentIndex = jest.fn();
     const initialIndex = 0;
 
-    const { container } = render(
+    const { container, getByTestId } = render(
       <Carousel
         images={images}
         setCurrentIndex={setCurrentIndex}
         initialIndex={initialIndex}
       />
     );
+
+    await waitFor(() => {
+      expect(getByTestId("image1.jpg")).toBeInTheDocument();
+      expect(getByTestId("image2.jpg")).toBeInTheDocument();
+    });
 
     expect(container).toMatchSnapshot();
   });
