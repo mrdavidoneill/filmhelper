9007da147debb79ee04e26094b395ed2f5ff5777
David ONeill <mrdavidoneillll@gmail.com>
2023-02-18 11:30:07 +0100

    Add animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,40 @@ module.exports = {
   ],
   important: "#__next",
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadein: "fadein 0.5s ease-in-out",
+        fadeinslow: "fadein 1s ease-in-out",
+        drop: "drop 0.5s ease-in-out",
+        risefast: "rise 0.2s ease-in-out",
+        rise: "rise 0.5s ease-in-out",
+        riseslow: "rise 1s ease-in-out",
+        goleft: "goleft 0.5s ease-in-out",
+        goright: "goright 0.5s ease-in-out",
+      },
+      keyframes: {
+        fadein: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "100%" },
+        },
+        drop: {
+          "0%": { transform: "translate(0vh, -100vh)" },
+          "100%": { transform: "translate(0vh, 0vh)" },
+        },
+        rise: {
+          "0%": { transform: "translate(0vh, 100vh)" },
+          "100%": { transform: "translate(0vh, 0vh)" },
+        },
+        goleft: {
+          "0%": { transform: "translate(100vh, 0vh)" },
+          "100%": { transform: "translate(0vh, 0vh)" },
+        },
+        goright: {
+          "0%": { transform: "translate(-100vh, 0vh)" },
+          "100%": { transform: "translate(0vh, 0vh)" },
+        },
+      },
+    },
   },
   plugins: [],
   corePlugins: {
