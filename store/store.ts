2ff32274024353a4dc58eb82bc0f7207c4285906
David ONeill <mrdavidoneillll@gmail.com>
2023-02-20 18:36:50 +0100

    Add ratings slice in reducer

--- store/store.ts
+++ store/store.ts
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { createLogger } from "redux-logger";
 import { createWrapper } from "next-redux-wrapper";
 import watchListSlice, { WatchListState } from "./slices/watchListSlice";
+import ratingsSlice, { RatingsState } from "./slices/ratingsSlice";
 
 const logger = createLogger();
 
 export interface RootState {
   watchList: WatchListState;
+  ratings: RatingsState;
 }
 
 const makeStore = () =>
@@ -15,6 +17,7 @@ const makeStore = () =>
     middleware: [logger],
     reducer: {
       watchList: watchListSlice.reducer,
+      ratings: ratingsSlice.reducer,
     },
   });
 
