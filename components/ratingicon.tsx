230bae09b8f29c9c0ae1249db9ea843dd9a69091
David ONeill <mrdavidoneillll@gmail.com>
2023-02-20 18:28:20 +0100

    Add onChange functionality - to handle rating changes - convert from % to stars and vice versa

--- components/ratingicon.tsx
+++ components/ratingicon.tsx
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { SyntheticEvent } from "react";
 import Rating from "@mui/material/Rating";
 import Stack from "@mui/material/Stack";
 
-export default function RatingIcon({ stars }: { stars: number }) {
+const NUMBER_OF_STARS = 5;
+const conversionRate = 100 / NUMBER_OF_STARS;
+
+function percentageToStars(percentage = 0) {
+  return percentage / conversionRate;
+}
+
+function starsToPercentage(stars = 0) {
+  return stars * conversionRate;
+}
+
+interface Props {
+  percentage: number;
+  onChange: (value: number) => void;
+}
+
+export default function RatingIcon({ percentage, onChange }: Props) {
+  function handleChange(
+    _event: SyntheticEvent<Element, Event>,
+    value: number | null
+  ) {
+    onChange(starsToPercentage(value || 0));
+  }
   return (
     <Stack spacing={1}>
-      <Rating name="half-rating" defaultValue={stars} precision={0.5} />
+      <Rating
+        name="half-rating"
+        value={percentageToStars(percentage)}
+        precision={0.5}
+        onChange={handleChange}
+      />
     </Stack>
   );
 }
